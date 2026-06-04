af4965af7ca7999fa161dc2be7d32b82af887694
Francisco Ibarra <56valle89@gmail.com>
2026-06-04 10:54:49 -0600

    feat: remove unused check scripts for correlativo, database, orders, users, and SMTP

--- check-correlativo.ts
+++
@@ -1,1 +0,0 @@
-import { PrismaClient } from '@prisma/client'; const prisma = new PrismaClient(); async function run() { const c = await prisma.correlativo.findMany(); console.log(c); } run();
--- check-db.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.appConfig.findUnique({ where: { id: 'floors_config' } })
-  .then(res => console.log(JSON.stringify(res, null, 2)))
-  .catch(console.error)
-  .finally(() => prisma.$disconnect());
