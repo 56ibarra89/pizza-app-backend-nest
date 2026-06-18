4429ce4ababa1f94f4ad570fd69ed39612234ca5
Francisco Ibarra <56valle89@gmail.com>
2026-06-17 19:19:10 -0600

    Chore: Register `OrderNotificationsListener` in `OrdersModule`

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -6,12 +6,15 @@ import { OrdersService } from './services/orders.service';
 import { ProductsModule } from '../products/products.module';
 import { AppConfigModule } from '../app-config/app-config.module';
 import { PromotionsModule } from '../promotions/promotions.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { OrderNotificationsListener } from './listeners/order-notifications.listener';
 
 @Module({
-  imports: [ProductsModule, AppConfigModule, PromotionsModule],
+  imports: [ProductsModule, AppConfigModule, PromotionsModule, MailerModule],
   controllers: [OrdersController],
   providers: [
     OrdersService,
+    OrderNotificationsListener,
     {
       provide: ORDERS_REPOSITORY,
       useClass: PrismaOrdersRepository,
